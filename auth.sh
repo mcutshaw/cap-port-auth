a6b93d2e435ba4220bcd8b5c975101af0f4af4a2
jackofnotrades <mike.cutshaw@hotmail.com>
2019-02-07 16:28:14 -0600

    removed replaced script

--- auth.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-read -p "Username: " AUTH_USERNAME
-stty -echo
-read -p "Password: " AUTH_PASSWORD
-stty echo
-curl 'https://captiveportal.ialab.dsu.edu:6082/php/uid.php?vsys=1&rule=2' -d 'inputStr=&escapeUser=&preauthid=&user='$AUTH_USERNAME'&passwd='$AUTH_PASSWORD'&ok=Login'
